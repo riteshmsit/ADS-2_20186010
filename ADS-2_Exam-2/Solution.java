74ea526b64a66c7d9050fd4b283310fc6242145e
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-11-10 11:04:09 +0530

    "ads2_m12_a1_1: 2/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2_Exam-2/Solution.java
+++ ADS-2_Exam-2/Solution.java
@@ -15,7 +15,7 @@ public class Solution {
 			String[] tokens = sc.nextLine().split(" ");
 			edge.addEdge(new Edge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]),Double.parseDouble(tokens[2])));
 		}
-		String caseToGo = null;
+		String caseToGo = sc.nextLine();
 		switch (caseToGo) {
 		case "Graph":
 			//Print the Graph Object.
