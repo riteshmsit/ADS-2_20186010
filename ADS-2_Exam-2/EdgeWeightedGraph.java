dd22798ba09c5d68a3cb8a1331d8787bc6d68e12
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-11-10 10:43:58 +0530

    "ads2_m12_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2_Exam-2/EdgeWeightedGraph.java
@@ -0,0 +1,106 @@
+/**.
+ * Class for edge weighted graph.
+ */
+class EdgeWeightedGraph {
+    /**.
+     * { var_description }
+     */
+    private int ver;
+    /**.
+     * { var_description }
+     */
+    private int edg;
+    /**.
+     * { var_description }
+     */
+    private double weight;
+    /**.
+     * { var_description }
+     */
+    private Bag<Edge>[] adj;
+    /**.
+     * Constructs the object.
+     *
+     * @param      v     { parameter_description }
+     */
+    EdgeWeightedGraph(final int v) {
+        this.ver = v;
+        adj = (Bag<Edge>[]) new Bag[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new Bag<Edge>();
+        }
+    }
+    /**.
+     * Adds an edge.
+     * TIME complexity in average case is 1.
+     * @param      e     { parameter_description }
+     */
+    public void addEdge(final Edge e) {
+        int v = e.either();
+        int w = e.other(v);
+        adj[v].add(e);
+        adj[w].add(e);
+        edg++;
+    }
+    /**.
+     * { function_description }
+     * TIME complexity in average case is 1.
+     * @return     { description_of_the_return_value }
+     */
+    public int vertices() {
+        return ver;
+    }
+    /**.
+     * { function_description }
+     * TIME complexity in average case is 1.
+     * @return     { description_of_the_return_value }
+     */
+    public int edg() {
+        return edg;
+    }
+    /**.
+     * { function_description }
+     * TIME complexity in average case is 1.
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<Edge> adj(final int v) {
+        // validateVertex(v);
+        return adj[v];
+    }
+    /**.
+     * { function_description }
+     * TIME complexity in average case is 1.
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int degree(final int v) {
+        return adj[v].size();
+    }
+    /**.
+     * { function_description }
+     * TIME complexity is no of edges.
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<Edge> edges() {
+        Bag<Edge> list = new Bag<Edge>();
+        for (int i = 0; i < ver; i++) {
+            int selfLoops = 0;
+            for (Edge ed : adj(i)) {
+                if (ed.other(i) > i) {
+                    list.add(ed);
+                } else if (ed.other(i) == i) {
+                    if (selfLoops % 2 == 0) {
+                        list.add(ed);
+                    }
+                    selfLoops++;
+                }
+            }
+        }
+        return list;
+    }
+    
+}
+
---
+++ ADS-2_Exam-2/Solution.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+		// Self loops are not allowed...
+		// Parallel Edges are allowed...
+		// Take the Graph input here...
+		Scanner sc = new Scanner(System.in);
+		int cities = sc.nextInt();
+		int roadlines = sc.nextInt();
+		sc.nextLine();
+		EdgeWeightedGraph edge = new EdgeWeightedGraph(cities);
+		for (int i = 0; i < roadlines; i++) {
+			String[] tokens = sc.nextLine().split(" ");
+			edge.addEdge(new Edge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]),Double.parseDouble(tokens[2])));
+		}
+		String caseToGo = null;
+		switch (caseToGo) {
+		case "Graph":
+			//Print the Graph Object.
+			System.out.println(edge);
+			break;
+
+		case "DirectedPaths":
+			// Handle the case of DirectedPaths, where two integers are given.
+			// First is the source and second is the destination.
+			// If the path exists print the distance between them.
+			// Other wise print "No Path Found."
+			
+			break;
+
+		case "ViaPaths":
+			// Handle the case of ViaPaths, where three integers are given.
+			// First is the source and second is the via is the one where path should pass throuh.
+			// third is the destination.
+			// If the path exists print the distance between them.
+			// Other wise print "No Path Found."
+			
+			break;
+
+		default:
+			break;
+		}
+	}
+}
